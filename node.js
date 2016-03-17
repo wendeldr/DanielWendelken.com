32341bb617f2027774b64841a6aee5ae995505a4
wendeldr <wendeldr@mail.uc.edu>
2016-03-17 18:22:40 -0400

    Fixed sendfile deprecation

--- node.js
+++ node.js
@@ -8,11 +8,10 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
-
 app.use(favicon(__dirname + '/public/img/icon.ico'));
 
 app.get('/', function(req, res) {
-  res.sendfile('./views/DanielWendelken.html');
+  res.sendFile('./views/DanielWendelken.html');
 });
 
 http.createServer(app).listen(port, function() {
