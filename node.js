274eb440b17c2a339ad07b827640731c71eaa4d1
Ubuntu <ubuntu@ip-172-31-56-22.ec2.internal>
2016-03-17 22:09:13 +0000

    Running with nodemon

--- node.js
+++ node.js
@@ -1,15 +1,15 @@
 var http = require('http'),
   express = require('express'),
   path = require('path'),
+  favicon = require('serve-favicon'),
   port = 9000;
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
-app.use(express.body-parser());
-app.use(express.methodOverride());
-app.use(express.serve-favicon(__dirname + '/public/img/icon.ico'));
+
+app.use(favicon(__dirname + '/public/img/icon.ico'));
 
 app.get('/', function(req, res) {
   res.sendfile('./views/DanielWendelken.html');
