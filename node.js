4e53d0e48d5df53cc79bfb2405c1199edc469ef5
wendeldr <wendeldr@mail.uc.edu>
2016-03-17 17:59:29 -0400

    fixed middleware favicon

--- node.js
+++ node.js
@@ -7,10 +7,9 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
-app.use(express.logger('dev'));
 app.use(express.body-parser());
 app.use(express.methodOverride());
-app.use(express.favicon(__dirname + '/public/img/icon.ico'));
+app.use(express.serve-favicon(__dirname + '/public/img/icon.ico'));
 
 app.get('/', function(req, res) {
   res.sendfile('./views/DanielWendelken.html');
