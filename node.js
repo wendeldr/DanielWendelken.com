30a443a157e0f38c1cd35158a3acefdf2d2b6759
wendeldr <wendeldr@mail.uc.edu>
2016-03-30 18:48:49 -0400

    update

--- node.js
+++ node.js
@@ -13,6 +13,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/DanielWendelken.html'));
 });
 
+app.get('/secret', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/FrontPage.html'));
+});
+
+app.get('/resume', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/resume.html'));
+});
+
 app.get('/resume/download', function(req, res){
   var file = __dirname + '/public/files/2016-Resume-DanielWendelken.pdf';
   res.download(file); // Set disposition and send it.
