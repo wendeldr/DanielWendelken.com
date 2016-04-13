077f78d520ec7fea77f16a4f408451e5d7694639
wendeldr <wendeldr@mail.uc.edu>
2016-04-13 06:06:20 +0100

    Lazygit auto commit

--- node.js
+++ node.js
@@ -10,11 +10,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(__dirname + '/public/img/icon.ico'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/DanielWendelken.html'));
+  res.sendFile(path.join(__dirname, 'public/FrontPage.html'));
 });
 
 app.get('/secret', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/FrontPage.html'));
+  res.sendFile(path.join(__dirname, 'public/DanielWendelken.html'));
 });
 
 app.get('/resume', function(req, res) {
@@ -24,6 +24,10 @@ app.get('/resume', function(req, res) {
 app.get('/resume/download', function(req, res){
   var file = __dirname + '/public/files/2016-Resume-DanielWendelken.pdf';
   res.download(file); // Set disposition and send it.
+});
+
+app.get('/about', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/about.html'));
 });
 
 http.createServer(app).listen(port, function() {
